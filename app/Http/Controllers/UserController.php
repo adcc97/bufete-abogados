1f528fa29214bb9e0de225c1304f1069fb8a2b29
DavidCastillo <adcc.97@outlook.com>
2021-09-04 16:45:35 -0500

    Instalacion de laravel y creación de CRUD para clientes

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(){
+        $users = User::latest()->get();
+
+        return view('users.index',[
+            'users' => $users
+        ]);
+
+    }
+
+    public function store(Request $request){
+        User::create([
+            'name' => $request->name,
+            'email' => $request->name,
+            'password' => $request->password,
+            'direccion' => $request->direccion,
+            'telefono' =>$request->telefono,
+
+        ]);
+        return back();
+    }
+
+    public function destroy(User $user){
+        $user->delete();
+        return back();
+    }
+}
